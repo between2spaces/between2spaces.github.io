0cc1fa95f99bf00734a35d0a555c286a9e103b1c
Between2Spaces <between2spaces@gmail.com>
2019-03-08 15:49:32 +1100

    Removing package.json info from morgue area due to lodash security warning

--- 1d/README.md
+++
@@ -1,8 +0,0 @@
-# 1d
-
-A game in one dimension.
-
-[▶ Play](1d.html)
-
-
-[🗎 1d.js](1d.js)
--- 1d/rollup.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    input: 'src/main.js',
-    indent: '\t',
-    sourcemap: 'inline',
-    output: {
-        file: 'bundle.js',
-        format: 'iife'
-    }
-};
