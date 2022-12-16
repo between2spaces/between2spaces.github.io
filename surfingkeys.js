a32b2521776834f8ca5a7f56295788d1d03aaa06
between2spaces <between2spaces@gmail.com>
2022-12-16 19:08:49 +1100

    asdf

--- surfingkeys.js
+++
@@ -1,2 +0,0 @@
-api.Hints.setCharacters('asdfgqwertyuiopzxcvbnm');
-api.addSearchAlias( "r", "Reddit Search", "https://www.reddit.com/search/?q=" );
--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git pull
-git status
-git add .
-git commit -m "sync"
-git push
-
