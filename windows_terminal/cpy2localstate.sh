50978c8c2f278f11746d916c2fa3da3dbfab5320
between2spaces <between2spaces@gmail.com>
2022-12-17 19:43:38 +1100

    lazy commit

--- windows_terminal/cpy2localstate.sh
+++ windows_terminal/cpy2localstate.sh
@@ -15,5 +15,5 @@ fi
 WINDOWS_TERMINAL="$LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/"
 
 echo "cp settings.json \"$WINDOWS_TERMINAL/LocalState/\""
-#cp settings.json "$WINDOWS_TERMINAL/LocalState/"
+cp settings.json "$WINDOWS_TERMINAL/LocalState/"
 
