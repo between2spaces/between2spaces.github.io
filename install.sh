17dba582132bc4534c068f219b7c297832cf2ed4
between2spaces <between2spaces@gmail.com>
2023-04-29 07:44:24 +1000

    lazy commit

--- install.sh
+++ install.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 set -e
-if [[ -d "~/between2spaces.github.io" ]]
+if [ $# -eq 0 ];
 then
-	echo "Updating existing clone..."
-	cd ~/between2spaces.github.io
-	git pull
-else
-	echo "Cloning github repo..."
-	cd ~
-	git clone https://github.com/between2spaces/between2spaces.github.io.git
-	cd ~/between2spaces.github.io
+	if [[ -d "~/between2spaces.github.io" ]];
+	then
+		echo "Updating existing clone..."
+		cd ~/between2spaces.github.io
+		git pull
+	else
+		echo "Cloning github repo..."
+		cd ~
+		git clone https://github.com/between2spaces/between2spaces.github.io.git
+		cd ~/between2spaces.github.io
+	fi
+	./install.sh update
+	exit 0
 fi
+
+
+echo "Configuring Git"
+git config --global credential.helper "store"
+
+git config user.email "between2spaces@gmail.com"
+git config user.name "between2spaces"
+
+
+
+if [ -z "$(grep -i micrxosoft /proc/version)" ]; then
+	echo "Running on WSL"
+	echo "Setting USERPROFILE environment variable"
+	cmd.exe /c setx WSLENV USERPROFILE/up 2>/dev/null
+	#export USERPROFILE=$(wslpath $(echo "$(cmd.exe /Q /C "echo %userprofile%" 2>/dev/null)" | sed "s/\r$//"))
+fi
+
+
