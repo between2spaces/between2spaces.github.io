d4b54b5e9996cad5f7fc0441d89995502d1a00ed
between2spaces <between2spaces@gmail.com>
2023-04-29 07:32:12 +1000

    lazy commit

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 set -e
 if [ -d "~/between2spaces.github.io" ]
 then
+	echo "Updating existing clone..."
 	cd ~/between2spaces.github.io
 	git pull
 else
+	echo "Cloning github repo..."
 	cd ~
 	git clone https://github.com/between2spaces/between2spaces.github.io.git
 	cd ~/between2spaces.github.io
