8ce3aa3902527c5ad56c66d7c1481b37795b1fb8
between2spaces <between2spaces@gmail.com>
2023-09-12 06:30:31 +1000

    lazy commit

--- docs/pwg/server.js
+++ docs/pwg/server.js
@@ -2,170 +2,6 @@ import { WebSocketServer } from 'ws';
 import crypto from 'crypto';
 import * as url from 'node:url';
 
-export let serverInstance;
-
-export class Entity {
-
-	constructor( args = {} ) {
-
-		args.type = 'type' in args ? args.type : this.constructor.name;
-
-		this.id = 'id' in args ? args.id : `${args.type}-${Server.uuid()}`;
-
-		Entity.byId[ this.id ] = this;
-
-		// set properties of the new Entity
-		for ( const property of Object.keys( args ) ) {
-
-			property !== 'id' && this.setProperty( property, args[ property ] );
-
-		}
-
-	}
-
-	setProperty( property, value ) {
-
-		// if no change to property value, do nothing
-		if ( this[ property ] === value ) return;
-
-		// if parentId is changing, remove entity from existing parentId list
-		if ( property === 'parentId' && this.parentId in Entity.byParentId ) {
-
-			const index = Entity.byParentId[ this.parentId ].indexOf( this );
-			if ( index > - 1 ) Entity.byParentId[ this.parentId ].splice( index, 1 );
-
-		}
-
-		// if type is changing, remove entity from existing type:id map
-		if ( property === 'type' && this.type in Entity.byType ) {
-
-			const entitiesOfType = Entity.byType[ this.type ];
-			const index = entitiesOfType.indexOf( this );
-
-			if ( index > - 1 ) entitiesOfType.splice( index, 1 );
-
-		}
-
-		// create a dirty map if not already marked dirty
-		if ( ! ( this.id in Entity.dirtyById ) ) Entity.dirtyById[ this.id ] = {};
-
-		// assign property value and delta
-		this[ property ] = Entity.dirtyById[ this.id ][ property ] = value;
-
-		// if parentId has changed, add entity to parentId list
-		if ( property === 'parentId' && this.parentId ) {
-
-			if ( ! ( this.parentId in Entity.byParentId ) ) Entity.byParentId[ this.parentId ] = [];
-			Entity.byParentId[ this.parentId ].push( this );
-
-		}
-
-		// if type has changed, add entity to type:id map
-		if ( property === 'type' ) {
-
-			if ( ! ( this.type in Entity.byType ) ) Entity.byType[ this.type ] = [];
-			const byType = Entity.byType[ this.type ];
-			const index = byType.indexOf( this.type );
-			if ( index === - 1 ) byType.push( this );
-
-		}
-
-	}
-
-	purge() {
-
-		delete Entity.byId[ this.id ];
-
-		if ( this.id in Entity.byParentId ) {
-
-			for ( let entity in Entity.byParentId[ this.id ] ) {
-
-				entity.setProperty( 'parentId', this.parentId );
-
-			}
-
-			delete Entity.byParentId[ this.id ];
-
-		}
-
-		const entitiesOfType = Entity.byType[ this.type ];
-		const index = entitiesOfType.indexOf( this );
-		if ( index > - 1 ) entitiesOfType.splice( index, 1 );
-
-		delete Entity.dirtyById[ this.id ];
-
-		serverInstance.send( {
-			from: 'server',
-			to: 'type=Client',
-			_: 'purge',
-			id: this.id
-		} );
-
-		console.log( `${this.id} purged.` );
-
-	}
-
-	update() {
-
-	}
-
-}
-
-Entity.byId = {};
-Entity.byParentId = {};
-Entity.byType = {};
-Entity.dirtyById = {};
-
-
-
-
-
-export class Client extends Entity {
-
-	constructor( args = {} ) {
-
-		super( args );
-
-	}
-
-	disconnect() {
-
-		serverInstance.onDisconnect( this );
-
-		this.purge();
-
-	}
-
-	update() {
-
-		super.update();
-
-	}
-
-	_send( msg ) {
-
-		const string = JSON.stringify( msg );
-		console.log( `${this.id}@ws <- ${string}` );
-		serverInstance.infoById[ this.id ].ws.send( string );
-
-	}
-
-	_undefined( msg ) {
-
-		this._send( msg );
-
-	}
-
-	_debug() {
-
-		this.disconnect();
-
-	}
-
-}
-
-
-
 
 
 export class Server {
@@ -175,18 +11,38 @@ export class Server {
 		this.allowedOrigins = args.allowedOrigins ? args.allowedOrigins : [ 'http://localhost:8000' ];
 		this.heartbeat = 'heartbeat' in args ? args.heartbeat : 666;
 		this.clientTimeout = 'clientTimeout' in args ? args.clientTimeout : 10000;
+
+		this.entityById = {}; // { entity, children }
+		this.entitiesByType = {};
+		this.dirtyEntityById = {};
+
+		this.clientById = {}; // { ws, secret, client, lastseen }
 		this.clientBySecret = {};
-		this.adminClientById = {};
-		this.infoById = {};
+
 		this.messages = [];
 
+		this.listenersByType = {};
+
+		this.listen( 'Entity', 'update', entity => {} );
+		this.listen( 'Client', 'update', client => this.call( 'server', 'Entity', client, 'update' ) );
+		this.listen( 'Client', 'message', ( client, message ) => {
+
+			const string = JSON.stringify( message );
+			console.log( `${this.id}@ws <- ${string}` );
+			serverInstance.clientById[ client.id ].ws.send( string );
+
+		} );
+
 		serverInstance = this;
+
+	}
+
+	run() {
 
 		const wss = new WebSocketServer( {
 			port: process.env.PORT,
 			verifyClient: info => this.allowedOrigins.indexOf( info.req.headers.origin ) > - 1
 		} );
-
 
 		wss.on( 'connection', ( ws, req ) => {
 
@@ -199,20 +55,57 @@ export class Server {
 
 			} else {
 
-				client = this.createClient();
+				client = this.createEntity( { type: 'Client' } );
 				secret = Server.uuid();
 
 			}
+
+			this.clientById[ client.id ] = { ws, secret, client, lastseen: Date.now() };
+
+			this.send( {
+				from: 'server',
+				to: client.id,
+				_: 'connected',
+				id: client.id,
+				secret: secret,
+				clientTimeout: this.clientTimeout,
+				serverHeartbeat: this.heartbeat
+			} );
+
+			if ( secret in this.clientBySecret ) {
+
+				console.log( `${client.id} reconnected` );
+
+			} else {
+
+				console.log( `${client.id} connected` );
+				this.clientBySecret[ secret ] = client;
+
+			}
+
+			this.onConnected( client );
+
+			// tell the connected Client about all Entities
+			for ( let id in this.entityById ) {
+
+				this.send( { from: 'server', to: client.id, _: 'entity', entity: this.entityById[ id ] } );
+
+			}
+
 
 			ws.on( 'message', data => {
 
 				try {
 
-					if ( ! ( client.id in this.infoById ) ) return ws.close();
+					if ( ! ( client.id in this.clientById ) ) return ws.close();
 
-					this.infoById[ client.id ].lastseen = Date.now();
+					this.clientById[ client.id ].lastseen = Date.now();
 
-					if ( `${data}` === 'undefined' ) return;
+					if ( `${data}` === 'undefined' ) {
+
+						return console.log( "ws.on( 'message', undefined ) received" );
+
+					}
 
 					let msgs = JSON.parse( data );
 
@@ -236,77 +129,61 @@ export class Server {
 
 			} );
 
-			this.infoById[ client.id ] = { ws, secret, client, lastseen: Date.now() };
-
-			this.send( {
-				from: 'server',
-				to: client.id,
-				_: 'connected',
-				id: client.id,
-				secret: secret,
-				clientTimeout: this.clientTimeout,
-				serverHeartbeat: this.heartbeat
-			} );
-
-			if ( secret in this.clientBySecret ) {
-
-				console.log( `${client.id} reconnected` );
-
-			} else {
-
-				console.log( `${client.id} connected` );
-				this.clientBySecret[ secret ] = client;
-
-			}
-
-			this.onConnect( client );
-
 		} );
 
-	}
-
-	run() {
-
-		console.log();
-		console.log( `Server listening on port ${process.env.PORT}` );
-		console.log();
-		console.log( `allowedOrigins: ${serverInstance.allowedOrigins}` );
-		console.log( `serverHeartbeat: ${serverInstance.heartbeat}` );
-		console.log( `clientTimeout: ${serverInstance.clientTimeout}` );
-		console.log();
-
-		this.scheduleNextUpdate();
+		this.requestUpdate();
 
 	}
 
-	scheduleNextUpdate() {
+	/**
+	 * Tells the server to schedule another update.
+	 *
+	 * @params timestampe <milliseconds elapsed since the epoch>
+	 */
+	requestUpdate( timestamp = Date.now() ) {
 
-		let timeout = Date.now() - this.lastUpdate;
+		let timeout = Date.now() - timestamp;
 
 		timeout = timeout > this.heartbeat ? 0 : this.heartbeat - timeout;
 
 		console.log( `next update in ${timeout}ms...` );
 
-		setTimeout( () => {
-
-			this.lastUpdate = Date.now();
-			serverInstance.update();
-
-		}, timeout );
+		setTimeout( () => serverInstance.update( Date.now() ), timeout );
 
 	}
 
-	update() {
-
-		if ( ! ( 'Client' in Entity.byType ) )
-			return this.scheduleNextUpdate();
+	/**
+	 * Updates the state of server.
+	 *
+	 * Stale Clients will be disconnected, queued messages will be delivered,
+	 * Entity delta's will be broadcast, dirty Entities will be updated.
+	 *
+	 * @param timestamp <milliseconds elapsed since the epoch>
+	 */
+	update( timestamp ) {
 
 		// disconnect clients we haven't heard from in awhile
-		const timeout = this.lastUpdate - this.clientTimeout;
+		const timeout = timestamp - this.clientTimeout;
 
-		for ( let client of Entity.byType[ 'Client' ] ) {
+		let client;
 
-			this.infoById[ client.id ].lastseen < timeout && client.disconnect();
+		for ( let id of this.clientById ) {
+
+			client = this.clientById[ id ];
+
+			if ( client.lastseen < timeout ) {
+
+				delete this.clientBySecret[ client.secret ];
+				delete this.clientById[ id ];
+				console.log( `${client.id} disconnected.` );
+				this.send( {
+					from: 'server',
+					to: id,
+					_: 'disconnected'
+				} );
+				this.onDisconnected( client );
+
+			}
 
 		}
 
@@ -317,7 +194,7 @@ export class Server {
 
 		for ( const msg of msgs ) {
 
-			const _ = `_${msg._}`;
+			const fn = `_${msg.fn}`;
 
 			let to = 'server';
 
@@ -338,7 +215,7 @@ export class Server {
 
 				const type = to.replace( 'type=', '' );
 
-				if ( ! ( type in Entity.byType ) ) {
+				if ( ! ( type in this.entitiesByType ) ) {
 
 					this.send( {
 						from: 'server',
@@ -351,11 +228,11 @@ export class Server {
 
 				}
 
-				targets = Entity.byType[ type ];
+				targets = this.entitiesByType[ type ];
 
 			} else {
 
-				if ( ! ( to in Entity.byId ) ) {
+				if ( ! ( to in this.entityById ) ) {
 
 					this.send( {
 						from: 'server',
@@ -377,15 +254,16 @@ export class Server {
 
 				target[ _ in target ? _ : '_undefined' ]( msg );
 
+				this.call( 'server', target.type, target, 'update' );
+
 			}
 
 		}
 
-
 		// broadcast entity deltas and call entity update
-		const dirtyById = Entity.dirtyById;
+		const dirtyById = this.dirtyEntityById;
 
-		Entity.dirtyById = {};
+		this.dirtyEntityById = {};
 
 		for ( const id in dirtyById ) {
 
@@ -398,94 +276,241 @@ export class Server {
 
 			}
 
-			Entity.byId[ id ].update();
+			this.entityById[ id ].entity.update();
 
-			if ( ! ( id in Entity.dirtyById ) ) console.log( `${id} went to sleep` );
-
-		}
-
-		this.scheduleNextUpdate();
-
-	}
-
-	createEntity( args ) {
-
-		return new Entity( args );
-
-	}
-
-	createClient( args ) {
-
-		return new Client( args );
-
-	}
-
-	onConnect( client ) {
-
-		// tell the connected client about all Entities
-		for ( let id in Entity.byId ) {
-
-			this.send( { from: 'server', to: client.id, _: 'entity', entity: Entity.byId[ id ] } );
+			if ( ! ( id in this.dirtyEntityById ) ) console.log( `${id} went to sleep` );
 
 		}
 
+		this.requestUpdate( timestamp );
+
 	}
 
-	onDisconnect( client ) {
+	/**
+	 * Creates a new Entity with the provided properties.
+	 *
+	 * @params properties object containing the properties you want to apply
+	 *
+	 * @returns The new Entity
+	 */
+	createEntity( properties = {} ) {
 
-		delete this.clientBySecret[ this.infoById[ client.id ].secret ];
-		delete this.infoById[ client.id ];
-		console.log( `${client.id} disconnected.` );
+		const entity = {};
+
+		entity.type = 'type' in properties ? properties.type : 'Entity';
+		entity.id = 'id' in properties ? properties.id : `${entity.type}-${Server.uuid()}`;
+
+		this.entityById[ entity.id ] = { entity, children: [] };
+
+		for ( const property of Object.keys( properties ) ) {
+
+			property !== 'id' && this.setProperty( entity, property, properties[ property ] );
+
+		}
+
+		return entity;
+
+	}
+
+	/**
+	 * Assigns a property and value to an Entity.
+	 *
+	 * @param entity The Entity to assign the property to
+	 * @param property The property to assign
+	 * @param value The value of the property
+	 *
+	 * @returns the new property value
+	 */
+	setProperty( entity, property, value ) {
+
+		// if no change to property value, do nothing
+		if ( entity[ property ] === value ) return entity[ property ];
+
+		//
+		// previous value cleanup steps....
+		//
+
+		// if parentId is changing, remove entity from existing parents list of children
+		if ( property === 'parentId' && entity.parentId in this.entityById ) {
+
+			const siblings = this.entityById[ entity.parentId ].children;
+			const index = siblings.indexOf( entity );
+			if ( index > - 1 ) siblings.splice( index, 1 );
+
+		}
+
+		// if type is changing, remove entity from types map
+		if ( property === 'type' && entity.type in this.entitiesByType ) {
+
+			const entitiesOfType = this.entitiesByType[ entity.type ];
+			const index = entitiesOfType.indexOf( entity );
+			if ( index > - 1 ) entitiesOfType.splice( index, 1 );
+
+		}
+
+		//
+		// new value asignment and flag dirty delta
+		//
+
+		// create a dirty map if not already marked dirty
+		if ( ! ( entity.id in this.dirtyEntityById ) ) this.dirtyEntityById[ entity.id ] = {};
+
+		// assign property value and add to delta
+		entity[ property ] = this.dirtyEntityById[ entity.id ][ property ] = value;
+
+		//
+		// new value implications...
+		//
+
+		// if parentId has changed, add entity to parents list of children
+		if ( property === 'parentId' && entity.parentId in this.entityById ) {
+
+			this.entityById[ entity.parentId ].children.push( entity );
+
+		}
+
+		// if type has changed, add entity to types map
+		if ( property === 'type' ) {
+
+			if ( ! ( entity.type in this.entitiesByType ) ) this.entitiesByType[ entity.type ] = [];
+			this.entitiesByType[ entity.type ].push( entity );
+
+		}
+
+		return entity[ property ];
+
+	}
+
+	/**
+	 * Deletes the specified Entity and broadcasts its removal to Clients.
+	 *
+	 * @param entity The Entity to delete
+	 */
+	purge( entity ) {
+
+		delete this.entitybyId[ entity.id ];
+
+		if ( entity.id in this.entitiesByParentId ) {
+
+			for ( let child in this.entitiesByParentId[ entity.id ] ) {
+
+				this.setProperty( child, 'parentId', entity.parentId );
+
+			}
+
+			delete this.entitiesByParentId[ entity.id ];
+
+		}
+
+		const entitiesOfType = this.entitiesByType[ entity.type ];
+		const index = entitiesOfType.indexOf( entity );
+
+		if ( index > - 1 ) entitiesOfType.splice( index, 1 );
+
+		delete this.dirtyEntityById[ entity.id ];
+
 		this.send( {
 			from: 'server',
-			to: client.id,
-			_: 'disconnected'
+			to: 'type=Client',
+			_: 'purge',
+			id: this.id
 		} );
+
+		console.log( `${this.id} purged.` );
+
+		this.call( 'server', entity, 'onPurged' );
 
 	}
 
-	send( msg ) {
+	/**
+	 * Queues the specified message for delivery.
+	 *
+	 * @param message A message object
+	 */
+	send( message ) {
 
-		if ( ! ( 'from' in msg ) ) return console.log( `Error: server.send past ${JSON.stringify( msg )} with no 'from'` );
+		if ( ! ( 'from' in message ) )
+			return console.log( `Error: server.send past ${JSON.stringify( message )} with no 'from'` );
 
-		if ( ! ( 'to' in msg ) ) {
+		if ( ! ( 'to' in message ) ) {
 
-			msg.value = `server.send past ${JSON.stringify( msg )} with no 'to'`;
-			msg.to = msg.from;
-			msg._ = 'log';
-			msg.level = 'error';
+			message.value = `server.send past ${JSON.stringify( message )} with no 'to'`;
+			message.to = message.from;
+			message._ = 'log';
+			message.level = 'error';
 
 		}
 
-		this.messages.push( msg );
+		this.messages.push( message );
+
+	}
+
+	/**
+	 * Registers a handler for the specifed Entity type and function name.
+	 *
+	 * @param entityType The Entity Type to associate the handler with
+	 * @param functionName The function name
+	 * @param fucntionHandler The handler to call
+	 */
+	listen( entityType, functionName, functionHandler ) {
+
+		if ( ! ( entityType in this.listenersByType ) ) this.listenersByType[ entityType ] = {};
+
+		this.listenersByType[ entityType ][ functionName ] = functionHandler;
+
+	}
+
+	/**
+	 * Calls the specified function on the specified target if defined, otherwise report an error.
+	 *
+	 * @param from The identity of the sender
+	 * @param targetObj The target object
+	 * @param functionName The name of the function on the object to call
+	 * @param arg The argument object to pass to the function
+	 *
+	 * @returns Return value of function
+	 */
+	call( from, targetObj, functionName, arg ) {
+
+		if ( ! ( targetObj.type in this.listenersByType ) || ! ( functionName in this.listenersByType[ targetObj.type ] ) ) {
+
+			this.send( {
+				from: 'server',
+				to: from,
+				_: 'log',
+				level: 'error',
+				value: `Unhandled call <${targetObj.type}>.${functionName}(...)`
+			} );
+
+		} else {
+
+			return this.listenersByType[ targetObj.type ][ functionName ]( arg );
+
+		}
+
+	}
+
+	/**
+	 * Called when a Client connects to the server.
+	 *
+	 * @param client The connected Client
+	 */
+	onConnected( client ) {
+
+	}
+
+	/**
+	 * Called when a Client disconnects from the server.
+	 *
+	 * @param client The disconnected Client
+	 */
+	onDisconnected( client ) {
 
 	}
 
 	_log( msg ) {
 
 		console.log( msg );
-
-	}
-
-	_undefined( msg ) {
-
-		try {
-
-			serverInstance.send( {
-				from: 'server',
-				to: msg.from,
-				_: 'log',
-				level: 'error',
-				value: `Unhandled message '${JSON.stringify( msg )}'`
-			} );
-
-		} catch ( e ) {
-
-			console.log( e.message );
-
-			console.log( msg );
-
-		}
 
 	}
 
@@ -504,12 +529,18 @@ Server.usedUUIDs = {};
 
 
 
+export let serverInstance;
+
 
 
 if ( url.fileURLToPath( import.meta.url ).replace( process.argv[ 1 ], '' ).replace( '.js', '' ) === '' ) {
 
 	// Main ESM module - path of this module matches path of module passed to node process
 
-	( new Server( { allowedOrigins: [ 'http://localhost:8000', 'https://between2spaces.github.io' ], heartbeat: 3333 } ) ).run();
+	( new Server( {
+		allowedOrigins: [ 'http://localhost:8000', 'https://between2spaces.github.io' ],
+		heartbeat: 3333
+	} ) ).run();
 
 }
+
