6bd925e2e45fef28a0687163d136b68e28b4b9c5
between2spaces <between2spaces@gmail.com>
2023-10-28 17:22:14 +1100

    lazy commit

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-set -e
-
-
 # Git config
 
 echo "Configuring Git"
@@ -31,6 +28,7 @@ fi
 
 
 
+set -e
 
 
 # WSL set WSLENV USERPROFile
@@ -71,7 +69,7 @@ fi
 
 # Full apt update, upgrade, remove, clean cycle
 
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
@@ -83,9 +81,21 @@ sudo apt autoclean -y
 sudo apt install openjdk-19-jdk
 
 
-# Python3 and pip
+# Python
 
-sudo apt install -y python3-pip
+# Ubuntu's default repositories missing latest version, so add deadsnakes open source repo
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt update -y
+sudo apt install python3.12
+
+
+
+
+
+exit 0
+
+
+
 
 
 # Powerline-shell
@@ -104,12 +114,14 @@ $HOME/between2spaces.github.io/terminal/update.sh
 
 echo "Node Version Manager"
 export NVM_DIR="$HOME/.nvm"
-if [ ! -d $NVM_DIR ]; then git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR" fi
+if [ ! -d $NVM_DIR ]; then
+	git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+fi
 
 CWD=$PWD
 cd "$NVM_DIR"
-git -c advice.detachedHead=false checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` .
-"$NVM_DIR/nvm.sh"
+git -c advice.detachedHead=false checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+. "$NVM_DIR/nvm.sh"
 cd $CWD
 
 nvm install node nvm install-latest-npm
@@ -134,7 +146,8 @@ if [ "$(which nvim)" == "" ] || [ "$(($(date +%s)-$(date -r $(which nvim) +%s)))
 
 	# Clone git repo; build and install
 	git clone https://github.com/neovim/neovim.git
-	cd neovim git checkout release-0.9
+	cd neovim
+	git checkout release-0.9
 	make CMAKE_BUILD_TYPE=Release
 	sudo make install
 
@@ -151,7 +164,9 @@ pip install pynvim
 # Telescope requirements
 sudo apt install ripgrep fd-find
 
-if [ ! -f ~/.local/bin/fd ]; then ln -s $(which fdfind) ~/.local/bin/fd fi
+if [ ! -f ~/.local/bin/fd ]; then
+	ln -s $(which fdfind) ~/.local/bin/fd
+fi
 
 # Clipboard support
 sudo apt install wl-clipboard
@@ -160,7 +175,9 @@ sudo apt install wl-clipboard
 # Neovim plugin manager
 echo "Neovim plugin manager"
 export LAZY_DIR="$HOME/.local/share/nvim/lazy/lazy.vim"
-if [ -d $LAZY_DIR ]; then rm -rf $LAZY_DIR fi
+if [ -d $LAZY_DIR ]; then
+	rm -rf $LAZY_DIR
+fi
 git clone --filter:blob:none https://github.com/folke/lazy.nvim.git --branch=stable $LAZY_DIR
 
 
