e522b2e5239273229416a22f5bf250c300683aca
between2spaces <between2spaces@gmail.com>
2024-04-30 16:44:29 +1000

    lazy commit

--- docs/map/client.js
+++ docs/map/client.js
@@ -1,6 +1,4 @@
-/**
- * 
- */
+import EntityTypeTemplate from './entitytypes.js';
 
 class Map {
 	constructor(container) {
@@ -8,124 +6,217 @@ class Map {
 		this.dom = document.createElement('div');
 		this.dom.className = 'view';
 		this.tiles = {};
+		this.tileSize = null;
+		const self = this;
+		window.addEventListener('resize', function (event) {
+			self.position();
+		}, true);
 	}
+
 	update() {
-		if (!this.tiles.hasOwnProperty('0x0')) return;
-		const tile = this.tiles['0x0'];
-		const tileWidth = tile.dom.offsetWidth;
-		const tileHeight = tile.dom.offsetHeight;
-		this.dom.style.left = `${window.innerWidth * 0.5 - tileWidth * 0.5 - this.offset.x * tileWidth}px`;
-		this.dom.style.top = `${window.innerHeight * 0.5 - tileHeight * 0.5 - this.offset.y * tileHeight}px`;
 	}
+
+	position(x = null, y = null) {
+		if (x === null) x = this.offset.x;
+		if (y === null) y = this.offset.y;
+		this.offset.x = x;
+		this.offset.y = y;
+		// (!this.tileSize) return;
+		this.dom.style.left = `calc(${window.innerWidth * 0.5}px - ${0.5 - this.offset.x}em)`;
+		this.dom.style.top = `calc(${window.innerHeight * 0.5}px - ${0.5 - this.offset.y}em)`;
+		//this.dom.style.top = `${window.innerHeight * 0.5 - 0.5 - this.offset.y * this.tileSize.height}px`;
+	}
+
+	centreNeighbour(direction = null) {
+		if (direction.indexOf('north') > -1) this.offset.y--;
+		else if (direction.indexOf('south') > -1) this.offset.y++;
+		if (direction.indexOf('east') > -1) this.offset.x++;
+		else if (direction.indexOf('west') > -1) this.offset.x--;
+		this.position();
+	}
+
 	add(entity) {
 		if (entity instanceof Tile) {
-			this.tiles[`${entity.offset.x}x${this.entity.y}`] = entity;
-		} else if (!entity.container) {
-			const tileX = Math.round(entity.offset.x);
-			const tileY = Math.round(entity.offset.y);
-			const key = `${tileX}x${tileY}`;
-			if (!this.tiles.hasOwnProperty(key)) {
-				this.tiles[key] = new Tile(tileX, tileY);
+			this.tiles[`${Math.round(entity.offset.x)}x${Math.round(entity.offset.y)}`] = entity;
+			const northKey = `${Math.round(entity.offset.x)}x${Math.round(entity.offset.y - 1)}`;
+			if (this.tiles.hasOwnProperty(northKey)) entity.assignNeighbour(this.tiles[northKey], 'north');
+			const southKey = `${Math.round(entity.offset.x)}x${Math.round(entity.offset.y + 1)}`;
+			if (this.tiles.hasOwnProperty(southKey)) entity.assignNeighbour(this.tiles[southKey], 'south');
+			const eastKey = `${Math.round(entity.offset.x + 1)}x${Math.round(entity.offset.y)}`;
+			if (this.tiles.hasOwnProperty(eastKey)) entity.assignNeighbour(this.tiles[eastKey], 'east');
+			const westKey = `${Math.round(entity.offset.x - 1)}x${Math.round(entity.offset.y)}`;
+			if (this.tiles.hasOwnProperty(westKey)) entity.assignNeighbour(this.tiles[westKey], 'west');
+			this.dom.append(entity.dom);
+			if (!this.tileSize) {
+				this.tileSize = { width: entity.dom.offsetWidth, height: entity.dom.offsetHeight };
+				this.position();
 			}
-			this.tiles[key].add(entity);
+		} else if (!entity.container) {
+			this.tileAt(entity).add(entity);
 		}
 	}
-	moveNorth() {
-		if (!this.tiles.hasOwnProperty(`${this.offset.x}x${this.offset.y - 1}`)) return;
-		this.offset.y--;
-		this.update();
-	}
-	moveSouth() {
-		this.offset.y++;
-		this.update();
-	}
-	moveWest() {
-		this.offset.x--;
-		this.update();
-	}
-	moveEast() {
-		this.offset.x++;
-		this.update();
+
+	tileAt(entity) {
+		const tileX = Math.round(entity.offset.x);
+		const tileY = Math.round(entity.offset.y);
+		const key = `${tileX}x${tileY}`;
+		if (!this.tiles.hasOwnProperty(key)) this.add(new Tile(tileX, tileY));
+		return this.tiles[key];
 	}
 }
 
-
-const EntityType = {
-	'ᚔ': {
-		name: 'fence',
-		width: 1,
-		height: 0.1
-	},
-	'"': {
-		name: 'grass',
-		width: 1,
-		height: 1
-	},
-}
 
 class Entity {
-	constructor(offsetx, offsety, type) {
-		this.offset = { x: offsetx, y: offsety };
-		this.type = type;
-		this.dom = document.createElement('div');
-		this.dom.className = 'entity';
-		this.dom.append(type);
+
+	constructor(type, x = 0, y = 0) {
+		this.offset = { x, y };
+		this.rotation = 0;
 		this.container = null;
 		this.contents = [];
+		Object.assign(this, EntityTypeTemplate[type]);
+		this.contents = [];
+		if (EntityTypeTemplate[type].hasOwnProperty('contents')) {
+			for (let child of EntityTypeTemplate[type].contents) {
+				this.add(new Entity)
+			}
+		}
+		this.dom = document.createElement('div');
+		this.dom.className = 'entity';
+		this.position(x, y);
+		this.rotate();
+		this.dom.append(type);
 	}
+
 	add(entity) {
 		if (entity.container) {
 			const i = entity.container.contents.indexOf(entity);
 			entity.container.contents.splice(i, 1);
 		}
+		entity.container = this;
 		this.contents.push(entity);
 		this.dom.append(entity.dom);
 	}
+
+	update() {
+	}
+
+	position(x = null, y = null) {
+		if (x === null) x = this.offset.x;
+		if (y === null) y = this.offset.y;
+		this.offset.x = x;
+		this.offset.y = y;
+		this.dom.style.left = `${this.offset.x}em`;
+		this.dom.style.top = `${this.offset.y}em`;
+	}
+
+	rotate(delta=0) {
+		this.rotation += delta;
+		this.dom.style.transform = `rotate(${this.rotation}deg)`;
+	}
+
 }
 
 
 class Tile extends Entity {
+
 	constructor(offsetx, offsety) {
-		super(offsetx, offsety, '"');
-		this.north = null;
-		this.east = null;
-		this.south = null;
-		this.west = null;
+		super('⛆', offsetx, offsety);
+		this.neighbour = {
+			north: null,
+			east: null,
+			south: null,
+			west: null
+		};
+		this.border = {
+			north: null,
+			east: null,
+			south: null,
+			west: null
+		};
 		this.dom.classList.add('tile');
-		map.dom.append(this.dom);
 	}
+
+	assignNeighbour(tile, direction) {
+		if (direction === 'north') {
+			this.neighbour.north = tile;
+			tile.neighbour.south = this;
+		} else if (direction === 'south') {
+			this.neighbour.south = tile;
+			tile.neighbour.north = this;
+		} else if (direction === 'east') {
+			this.neighbour.east = tile;
+			tile.neighbour.west = this;
+		} else if (direction === 'west') {
+			this.neighbour.west = tile;
+			tile.neighbour.east = this;
+		}
+	}
+
+	assignBorder(entity, direction) {
+		this.add(entity);
+		if (direction === 'north') {
+			this.border.north = entity;
+			entity.position(0, -0.5);
+		} else if (direction === 'south') {
+			this.border.south = entity;
+			entity.position(0, 0.49999);
+			entity.rotate(180);
+		} else if (direction === 'east') {
+			this.border.east = entity;
+			entity.position(0.49999, 0);
+			entity.rotate(90);
+		} else if (direction === 'west') {
+			this.border.west = entity;
+			entity.position(-0.5, 0);
+			entity.rotate(-90);
+		}
+	}
+
 }
 
-
-const map = new Map();
-
-
-function update() {
-	map.update();
-}
 
 
 export default function main(container) {
+	const map = new Map();
+
 	container.append(map.dom);;
 
-	const moveNorth = ['w', 'k', 'ArrowUp'];
-	const moveSouth = ['s', 'j', 'ArrowDown'];
-	const moveWest = ['a', 'h', 'ArrowLeft'];
-	const moveEast = ['d', 'l', 'ArrowRight'];
+	const moveMapKeys = {
+		'w': 'north',
+		'k': 'north',
+		'ArrowUp': 'north',
+		's': 'south',
+		'j': 'south',
+		'ArrowDown': 'south',
+		'a': 'west',
+		'h': 'west',
+		'ArrowLeft': 'west',
+		'd': 'east',
+		'l': 'east',
+		'ArrowRight': 'east'
+	};
 
 	window.addEventListener('keydown', (event) => {
-		if (moveNorth.indexOf(event.key) > -1) {
-			map.moveNorth();
-		} else if (moveSouth.indexOf(event.key) > -1) {
-			map.moveSouth();
-		} else if (moveWest.indexOf(event.key) > -1) {
-			map.moveWest();
-		} else if (moveEast.indexOf(event.key) > -1) {
-			map.moveEast();
+		if (moveMapKeys.hasOwnProperty(event.key)) {
+			map.centreNeighbour(moveMapKeys[event.key]);
 		}
 	});
 
-	map.add(new Entity(0, 0, 'ᚔ'));
+	map.add(new Tile(0, 0));
+	map.add(new Tile(0, -1));
+
+	
+	map.tiles['0x0'].assignBorder(new Entity('═'), 'east');
+	map.tiles['0x0'].assignBorder(new Entity('═'), 'south');
+	map.tiles['0x0'].assignBorder(new Entity('ᚔ'), 'west');
+
+	map.tiles['0x-1'].assignBorder(new Entity('🚪'), 'north');
+
+	function update() {
+		map.update();
+	}
 
 	setInterval(update, 1000);
+
+	console.log(map.tiles['0x0']);
 }
